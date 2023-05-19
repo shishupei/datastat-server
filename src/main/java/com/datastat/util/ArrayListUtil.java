df4a17f93e495ce84fc8174f736522036af21bbb
kaede10 <sch_na@163.com>
2023-05-19 17:50:57 +0800

    Add func getExtends

--- src/main/java/com/datastat/util/ArrayListUtil.java
+++ src/main/java/com/datastat/util/ArrayListUtil.java
@@ -23,4 +23,14 @@ public class ArrayListUtil {
         }
         return arrayList;
     }
+
+    public static <T> ArrayList<T> castList(Object obj, Class<T> clazz) {
+        ArrayList<T> result = new ArrayList<T>();
+        if (obj instanceof ArrayList<?>) {
+            for (Object o : (ArrayList<?>) obj) {
+                result.add(clazz.cast(o));
+            }
+        }
+        return result;
+    }
 }
