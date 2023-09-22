62cff9376a7cdd27f893d5cc08840d2eac495b08
gouzhonglin <gouzhonglin@huawei.com>
2023-09-22 14:58:13 +0800

    test trace

--- src/main/java/com/datastat/dao/QueryDao.java
+++ src/main/java/com/datastat/dao/QueryDao.java
@@ -78,6 +78,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
