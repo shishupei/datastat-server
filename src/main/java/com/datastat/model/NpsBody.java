4d13a2541e336f9c636a0244fab6567b477c52e2
kaede10 <sch_na@163.com>
2024-03-25 10:09:56 +0800

    update url format

--- src/main/java/com/datastat/model/NpsBody.java
+++ src/main/java/com/datastat/model/NpsBody.java
@@ -9,11 +9,11 @@ import lombok.Data;
 @Data
 public class NpsBody {
     @Size(max = 500,  message = "the length can not exceed 500")
-    @Pattern(regexp = "^[^<>{}[]]*$", message = "Text format error")
+    @Pattern(regexp = "^((http|https)://)[^<>{}\\[\\])]*$", message = "Url format error")
     private String feedbackPageUrl;
 
     @Min(value = 0, message = "Value must be greater than or equal to 0")
-    @Max(value = 10, message = "Value must be less than or equal to 0")
+    @Max(value = 10, message = "Value must be less than or equal to 10")
     private int feedbackValue;
 
     @Size(max = 500,  message = "the length can not exceed 500")
