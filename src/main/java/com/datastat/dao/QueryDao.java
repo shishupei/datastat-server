e463c1606fe9196274f0ff4c272dd715f74ef6e8
G-Sacker <ljx11714472@gmail.com>
2024-06-11 16:28:39 +0800

    test bug

--- src/main/java/com/datastat/dao/QueryDao.java
+++ src/main/java/com/datastat/dao/QueryDao.java
@@ -541,6 +541,7 @@ public class QueryDao {
         dataMap.put("company_cn", "个人贡献者");
         dataMap.put("company_en", "independent");
         dataMap.put("contribute", independent);
+        dataMap.put("query",contributesQueryStr);
         JsonNode resNode = objectMapper.valueToTree(dataMap);
         dataList.add(resNode);
 
