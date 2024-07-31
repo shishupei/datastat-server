26a39838384eac40b0812b6f36c4e536f76db205
kaede10 <sch_na@163.com>
2024-07-31 14:08:16 +0800

    delete aigc pr issue apis

--- src/main/java/com/datastat/model/CustomPropertiesConfig.java
+++ src/main/java/com/datastat/model/CustomPropertiesConfig.java
@@ -205,15 +205,6 @@ public class CustomPropertiesConfig {
     private String isvCountQuery;
     private String repoDeveloperQueryStr;
     private String viewCountQueryStr;
-    private String pullsQueryStr;
-    private String issueQueryStr;
-    private String pullsQueryReposStr;
-    private String pullsQueryAssigneesStr;
-    private String pullsQueryAuthorsStr;
-    private String pullsQueryRefsStr;
-    private String pullsQuerySigsStr;
-    private String pullsQueryLabelsStr;
-    private String issueQueryLabelsStr;
     private String coreRepo;
     private String sigGatheringUserCount;
     private String repoIssueField;
