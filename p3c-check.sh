173ca3f63586df7e74fda20601a29435739b4b3b
G-Sacker <ljx11714472@gmail.com>
2024-08-08 09:57:00 +0800

    修改p3c配置

--- p3c-check.sh
+++ p3c-check.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Download p3c.jar if it doesn't exist
+if [ ! -f "target/p3c.jar" ]; then
+  wget https://github.com/alibaba/p3c/releases/download/p3c-2.2.0/p3c.jar
+  mvn install:install-file -Dfile=p3c.jar -DgroupId=com.alibaba.p3c -DartifactId=p3c -Dversion=2.2.0 -Dpackaging=jar
+fi
+
+# Run P3C check
 mvn clean test pmd:pmd -Dpmd.rule.file=p3c.xml -Dpmd.exclude.pattern=**/target/**
