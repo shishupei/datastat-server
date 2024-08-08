7bb8761e8c86cec9d4e17cdeeebdc74dc8c02d1e
G-Sacker <ljx11714472@gmail.com>
2024-08-08 10:10:38 +0800

    更新p3c检查脚本和引入plexus-utils-3.5.0.jar

--- p3c-check.sh
+++ p3c-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download p3c.jar if it doesn't exist
-if [ ! -f "target/p3c.jar" ]; then
+if [ ! -f "static/p3c.jar" ]; then
   wget https://github.com/alibaba/p3c/releases/download/p3c-2.2.0/p3c.jar
   mvn install:install-file -Dfile=p3c.jar -DgroupId=com.alibaba.p3c -DartifactId=p3c -Dversion=2.2.0 -Dpackaging=jar
 fi
