9f8a8ef5b833e5d471a81132d24c5a4134cc7779
G-Sacker <ljx11714472@gmail.com>
2024-08-08 11:22:31 +0800

    更新P3C检查脚本及配置，引入p3c-core依赖，修改PMD规则

--- p3c-check.sh
+++ p3c-check.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Download p3c.jar if it doesn't exist
-if [ ! -f "static/p3c.jar" ]; then
-  wget https://github.com/alibaba/p3c/releases/download/p3c-2.2.0/p3c.jar
-  mvn install:install-file -Dfile=p3c.jar -DgroupId=com.alibaba.p3c -DartifactId=p3c -Dversion=2.2.0 -Dpackaging=jar
-fi
+# 进入 p3c 目录
+cd p3c
 
-# Run P3C check
+# 编译 P3C
+mvn clean install
+
+# 回到项目根目录
+cd ..
+
+# 运行 PMD 检查
 mvn clean test pmd:pmd -Dpmd.rule.file=p3c.xml -Dpmd.exclude.pattern=**/target/**
